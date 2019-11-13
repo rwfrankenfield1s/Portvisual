bd953c5e2a1e93c9d04e9970fb4a0e97d1592167
RW - Ross Frankenfield <rwfrankenfield1s@gmail.com>
2019-11-13 09:18:52 -0600

    Initial Load
    Establish on GH

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component'; // Add this
+import { IndependentComponent } from './independent/independent.component'; // Add this
+import { AdvisorComponent } from './advisor/advisor.component'; // Add this
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },              // Add this
+  { path: 'Independent', component: IndependentComponent },           // Add this
+  { path: 'Advisor', component: AdvisorComponent }           // Add this
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/independent/independent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-independent',
+  templateUrl: './independent.component.html',
+  styleUrls: ['./independent.component.scss']
+})
+export class IndependentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
